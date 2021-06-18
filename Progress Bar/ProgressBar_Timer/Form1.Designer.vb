086255ca879ee256921ba7eab1a242310be2faff
closeresty <85859112+closeresty@users.noreply.github.com>
2021-06-18 15:30:56 +0800

    Add files via upload
    This my project of progress bar using visual studio 2019

---
+++ Progress Bar/ProgressBar_Timer/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProgress
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.picShow = New System.Windows.Forms.PictureBox()
+        CType(Me.picShow, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ProgressBar1.Location = New System.Drawing.Point(0, 360)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(917, 25)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(354, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(527, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "SMART CLASS RFID BASE MONITORING SYSTEM "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(354, 119)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(538, 25)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "MANAGEMENT WITH SMS NOTIFICATION ONLINE"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Tai Le", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label3.Location = New System.Drawing.Point(356, 165)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "BY: Potolin, Resty A."
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Tai Le", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Label4.Location = New System.Drawing.Point(481, 165)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "-  Walking Dev"
+        '
+        'picShow
+        '
+        Me.picShow.Location = New System.Drawing.Point(12, 303)
+        Me.picShow.Name = "picShow"
+        Me.picShow.Size = New System.Drawing.Size(56, 51)
+        Me.picShow.TabIndex = 1
+        Me.picShow.TabStop = False
+        '
+        'frmProgress
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(917, 385)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.picShow)
+        Me.MaximizeBox = False
+        Me.Name = "frmProgress"
+        Me.Text = "Walking Dev"
+        CType(Me.picShow, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents picShow As PictureBox
+End Class
---
+++ Progress Bar/ProgressBar_Timer/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class frmProgress
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(10)
+        If ProgressBar1.Value = 20 Then
+            picShow.Image = My.Resources.wait_icon1
+        End If
+        If ProgressBar1.Value = 40 Then
+            picShow.Image = My.Resources.wait_icon2
+        End If
+        If ProgressBar1.Value = 60 Then
+            picShow.Image = My.Resources.wait_icon1
+        End If
+        If ProgressBar1.Value = 80 Then
+            picShow.Image = My.Resources.wait_icon2
+        End If
+        If ProgressBar1.Value = 100 Then
+            picShow.Image = My.Resources.wait_icon1
+        End If
+        If ProgressBar1.Value = ProgressBar1.Maximum Then
+            Timer1.Stop()
+            picShow.Dock = DockStyle.Fill
+            picShow.Image = My.Resources.CDM
+            picShow.SizeMode = PictureBoxSizeMode.Normal
+        End If
+    End Sub
+
+    Private Sub frmprogress_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+End Class
